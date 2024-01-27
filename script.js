18777102afa58f6bcb2db4cecd50c31253cd53bc
tjkue <156230037+tjkue@users.noreply.github.com>
2024-01-27 17:11:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,8 +12,6 @@ const [street, town, country] = locationValue.split(',');
 document.getElementById('name').innerText = nameValue;
 document.getElementById('email').innerHTML = `E-Mail: <a href="mailto:${emailValue}">${emailValue}</a>`;
 document.getElementById('phone').innerHTML = `Telefon: <a href="tel:${phoneValue.replace(/\s/g, '')}">${phoneValue}</a>`;
-//document.getElementById('email').innerText = `E-Mail: ${emailValue}`;
-//document.getElementById('phone').innerText = `Telefon: ${phoneValue}`;
 document.getElementById('street').innerText = street.trim();
 document.getElementById('town').innerText = town.trim();
 document.getElementById('country').innerText = country.trim();
